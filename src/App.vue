e75170878ed23907a22692e8ec5ec8381aeb5cd4
Dela Cruz, Michael Angelo <134262194+michinnn@users.noreply.github.com>
2023-10-24 09:56:57 +0800

    update-10-24

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
 <template>
 
 <!-- NAVIGATION BAR -->
-  <nav class="flex absolute top-0 left-0 right-0 justify-between items-center px-20 rounded-b-md bg-slate-100/70 shadow-md z-50">
+  <nav class="flex absolute top-0 left-0 right-0 justify-between h-16 items-center px-32 rounded-b-md bg-slate-100/70 shadow-md z-50">
     <div>
-      <img src="logo2.png" alt="logo" class="h-16">
+      <img src="logo2.png" alt="logo" class="h-10">
     </div>
     <div class="flex space-x-10 font-dmsans font-semibold text-pinkish gap-10">
       <a href="###">HOME</a>
@@ -20,31 +20,42 @@
 
 
   <!-- START OF BODY -->
-  <div class="flex relative w-full h-full items-center justify-between px-20">
-    <div style="background-image: url('maple.png');" class="h-[50vh] w-1/2 fixed right-0 top-0"> </div>
-    <div style="background-image: url('maple2.png'); background-repeat: no-repeat;" class="h-[50vh] w-1/2 fixed left-0 bottom-0"> 
-    </div>
-      <div class="flex w-full h-full border fixed top-0 right-0 left-0 bottom-0 px-20 justify-between py-40">
-        <div class="w-1/3">
-        <img src="profile.jpg" alt="profile" class="w-96 h-96 rounded-md z-40">
+  <div class="flex relative w-full h-full items-center justify-between px-32">
+
+    <!-- START OF MAPLE -->
+
+    <div style="background-image: url('maple.png'); background-repeat: no-repeat;" class="h-[50vh] w-1/2 fixed right-0 top-0"> </div>
+      <div style="background-image: url('maple2.png'); background-repeat: no-repeat; background-size: 100%;" class="h-1/2 w-[45vw] fixed left-0 bottom-0"> </div>
+   
+      <!-- END OF MAPLE -->
+
+      <div class="flex w-full h-full border fixed top-0 right-0 left-0 bottom-0 px-32 py-40">
+        <div class="flex justify-start items-center w-1/2 h-full">
+          <img src="profile.jpg" alt="profile" class="border-4 border-pinkish/50 h-4/5 rounded-md z-40">
         </div>
 
 
         <!-- START OF NAMES -->
 
-      <div class="flex flex-col justify-center text-pinkish text-5xl z-40 font-bold w-2/3 ">
-        <p class="leading-8">MARIA ANDRHEA IVHON</p>
-        <p class="leading-8 opacity-70">MARIA ANDRHEA IVHON</p>
-        <p class="leading-8 opacity-40">MARIA ANDRHEA IVHON</p>
-       <div>
-        <p class="text-base text-black font-medium pt-2">an aspiring front-end developer / UI/UX designer / Graphic Designer.</p>
-       </div>
+      <div class="flex flex-col justify-center items-end text-pinkish text-5xl z-40 font-bold w-1/2 h-full">
+
+        <div class="flex flex-col items-end w-fit">
+          <p class="leading-8">MARIA ANDRHEA IVHON</p>
+          <p class="leading-8 opacity-50">MARIA ANDRHEA IVHON</p>
+          <p class="leading-8 opacity-20">MARIA ANDRHEA IVHON</p>
+
+          <div class="flex w-full text-xs text-black font-medium pt-2">
+            <p>an aspiring front-end developer / UI/UX designer / Graphic Designer.</p>
+          </div>
+
+        </div>
+       
       </div>
 
       <!-- END OF NAMES -->
 
 
-  </div>
+    </div>
   </div>
   <!-- END OF BODY -->
 
