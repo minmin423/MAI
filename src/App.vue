5081efebd5cd76a8bbcdc3880b10fb354ac66d03
Dela Cruz, Michael Angelo <134262194+michinnn@users.noreply.github.com>
2023-10-24 14:11:03 +0800

    added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
-<script setup>
 
+<script setup>
+import Portfolio from './components/Portfolio.vue';
 </script>
 
 <template>
 
-<!-- NAVIGATION BAR -->
-  <nav class="flex absolute top-0 left-0 right-0 justify-between h-16 items-center px-32 rounded-b-md bg-slate-100/70 shadow-md z-50">
+<div class="w-full min-h-screen">
+  <!-- NAVIGATION BAR -->
+  <nav class="flex fixed top-0 left-0 right-0 justify-between h-16 items-center px-32 rounded-b-md bg-slate-100/70 shadow-md z-50">
     <div>
       <img src="logo2.png" alt="logo" class="h-10">
     </div>
@@ -20,16 +22,16 @@
 
 
   <!-- START OF BODY -->
-  <div class="flex relative w-full h-full items-center justify-between px-32">
+  <div class="flex relative w-full h-screen items-center justify-between px-32">
 
     <!-- START OF MAPLE -->
 
-    <div style="background-image: url('maple.png'); background-repeat: no-repeat;" class="h-[50vh] w-1/2 fixed right-0 top-0"> </div>
-      <div style="background-image: url('maple2.png'); background-repeat: no-repeat; background-size: 100%;" class="h-1/2 w-[45vw] fixed left-0 bottom-0"> </div>
+    <div style="background-image: url('maple.png'); background-repeat: no-repeat;" class="h-[50vh] w-[52vw] absolute right-0 top-0"> </div>
+    <div style="background-image: url('maple2.png'); background-repeat: no-repeat; background-size: 100%;" class="h-1/2 w-[41vw] absolute left-0 bottom-0"> </div>
    
       <!-- END OF MAPLE -->
 
-      <div class="flex w-full h-full border fixed top-0 right-0 left-0 bottom-0 px-32 py-40">
+      <div class="flex w-full h-full absolute top-0 right-0 left-0 bottom-0 px-32 py-40">
         <div class="flex justify-start items-center w-1/2 h-full">
           <img src="profile.jpg" alt="profile" class="border-4 border-pinkish/50 h-4/5 rounded-md z-40">
         </div>
@@ -47,19 +49,13 @@
           <div class="flex w-full text-xs text-black font-medium pt-2">
             <p>an aspiring front-end developer / UI/UX designer / Graphic Designer.</p>
           </div>
-
-        </div>
-       
+        </div>      
       </div>
-
       <!-- END OF NAMES -->
-
-
     </div>
   </div>
   <!-- END OF BODY -->
 
-
-
-
+  <Portfolio/>
+</div>
 </template>
---
+++ src/components/Portfolio.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="h-screen w-full">
+        
+        <div class="flex flex-col text-pinkish jusify-center items-center font-medium w-full text-[110px] px-56 py-10">
+                <p class="flex leading-3 opacity-20 justify-end w-full -mb-5">PORTFOLIO</p>
+                <div class="flex justify-center items-center relative w-[98vw] h-full">
+                    <div class="absolute left-0 h-2 bg-pinkish w-[450px]"></div>
+                    <p class="z-30 leading-none">PORTFOLIO</p>
+                    <div class="absolute right-0 h-2 bg-pinkish w-[450px]"></div>
+                </div>
+                <p class="flex leading-3 opacity-50 justify-start w-full -mt-5">PORTFOLIO</p>
+        </div>
+
+        <div class="w-full h-[71px] px-72 -mt-20 flex gap-2 justify-end items-end">
+            <div class="flex flex-col  font-medium">
+                <p class="text-black/50 hover:text-black z-50 cursor-pointer"
+                    v-for="(item, index) in menuItems"
+                    :key="index"
+                    @click="itemIndexClicked(index)"
+                    :class="{'text-black/100' : index == itemIndex}"
+                    >{{item}}</p>
+            </div>
+
+            <div class="flex flex-col py-1 h-full"
+                :class="{'justify-center': itemIndex == 1, 'justify-start': itemIndex == 0, 'justify-end': itemIndex == 2}">
+                <img src="heart.png" alt="heart" class="h-3 w-3">
+            </div>
+        </div>
+      
+    </div>
+
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    const menuItems = ref([
+        "Graphic Design", "UI Design", "Other Works"
+    ]);
+    const itemIndex = ref(0);
+
+    const itemIndexClicked = (index) => {
+        itemIndex.value = index;
+    };
+</script>
